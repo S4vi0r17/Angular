319bb8ff49c18a4072f416a3e8f36daa615b8be3
S4v!0r <gbp17@outlook.com>
2024-08-04 23:24:17 -0500

    feat: Add main page component with courses list and form components

---
+++ 01-my-app/src/app/courses/components/form/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
