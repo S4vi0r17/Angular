650ea97c139d3a47b69423c8e39c1ae6ab46f92e
S4v!0r <gbp17@outlook.com>
2024-08-16 11:51:23 -0500

    ```text refactor: Update searchGifs method in GifsService to organize search history
    The searchGifs method in GifsService has been updated to include logic for organizing the search history. If the provided tag is empty, the method returns without performing any actions. Otherwise, the tag is trimmed and converted to lowercase before being added to the history. If the tag already exists in the history, it is removed from its previous position and moved to the front. The history is then limited to a maximum of 10 items.

--- 02-giphy-app/src/app/gifs/services/gifs.service.ts
+++ 02-giphy-app/src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,22 @@ export class GifsService {
   }
 
   searchGifs(tag: string) {
+    if (tag.trim().length === 0) {
+      return;
+    }
+
+    this.organizeHistory(tag);
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.trim().toLowerCase();
+
+    if (this._history.includes(tag)) {
+      this._history = this._history.filter((item) => item !== tag);
+    }
+
     this._history.unshift(tag);
-    console.log(this._history);
+
+    this._history = this._history.splice(0, 10);
   }
 }
