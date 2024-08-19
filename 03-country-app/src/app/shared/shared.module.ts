356ceb0fb49f68e73916f0c0b74570c6964a433c
S4v!0r <gbp17@outlook.com>
2024-08-19 15:37:24 -0500

    Add initial project files and configurations

--- 03-country-app/src/app/shared/shared.module.ts
+++ 03-country-app/src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    AsideComponent,
+    ContactPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    AsideComponent
   ]
 })
 export class SharedModule { }
