cbfb413d11af4c6709a2eff4f80b9ee1bf2b8595
S4v!0r <gbp17@outlook.com>
2024-08-20 16:29:01 -0500

    Refactor aside component to add cursor pointer to list items

--- 03-country-app/src/app/shared/components/aside/aside.component.ts
+++ 03-country-app/src/app/shared/components/aside/aside.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-aside',
   templateUrl: './aside.component.html',
+  styles: [
+    `
+      .cursor-pointer {
+        cursor: pointer;
+        margin-block:.2rem;
+      }
+    `
+  ]
 })
 export class AsideComponent {
 
