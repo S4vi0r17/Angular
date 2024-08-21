e459b4b22a6ba623d4f80205b79122b162d15169
S4vi0r <gbp17@outlook.com>
2024-08-20 23:35:48 -0500

    Refactor app module to import HttpClientModule

---
+++ 03-country-app/src/app/countries/services/countries.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+
+@Injectable({ providedIn: 'root' })
+export class ServiceNameService {
+
+    apiUrl = 'https://restcountries.com/v3.1/capital/{capital}';
+
+    constructor(private httpClient: HttpClient) { }
+
+    seachCapital(query: string): Observable<Country[]> {
+        const url = this.apiUrl.replace('{capital}', query);
+        return this.httpClient.get<Country[]>(url);
+    }
+
+}
