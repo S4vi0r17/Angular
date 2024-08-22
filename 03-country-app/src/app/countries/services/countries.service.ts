793cf87a6b7ea59841c8917ea7a0cb5bea5087ba
S4vi0r <gbp17@outlook.com>
2024-08-21 22:45:42 -0500

    Refactor countries service to update API endpoints and add search functionality

--- 03-country-app/src/app/countries/services/countries.service.ts
+++ 03-country-app/src/app/countries/services/countries.service.ts
@@ -6,12 +6,26 @@ import { Country } from '../interfaces/country.interface';
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
 
-    apiUrl = 'https://restcountries.com/v3.1/capital/{capital}';
+    apiUrl = 'https://restcountries.com/v3.1';
 
     constructor(private httpClient: HttpClient) { }
 
     seachCapital(query: string): Observable<Country[]> {
-        const url = this.apiUrl.replace('{capital}', query);
+        const url = `${this.apiUrl}/capital/${query}`;
+        return this.httpClient.get<Country[]>(url).pipe(
+            catchError((error) => of([])),
+        );
+    }
+
+    searchCountry(query: string): Observable<Country[]> {
+        const url = `${this.apiUrl}/name/${query}`;
+        return this.httpClient.get<Country[]>(url).pipe(
+            catchError((error) => of([])),
+        );
+    }
+
+    searchRegion(region: string): Observable<Country[]> {
+        const url = `${this.apiUrl}/region/${region}`;
         return this.httpClient.get<Country[]>(url).pipe(
             catchError((error) => of([])),
         );
