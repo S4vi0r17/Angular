d970c2d9e05801466dc725f31fe78dbfada9de47
S4vi0r <gbp17@outlook.com>
2024-08-24 00:00:57 -0500

    Refactor search box component to implement debouncing for better user experience

--- 03-country-app-v2/src/app/shared/components/searchBox/searchBox.component.ts
+++ 03-country-app-v2/src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,16 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './searchBox.component.html',
   styleUrl: './searchBox.component.css',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>();
+
+  private debounceSubscription?: Subscription;
+
   @Input() placeholder: string = '';
 
   @Output() search = new EventEmitter<string>();
 
+  @Output() onDebounce = new EventEmitter<string>();
+
   emitValue(value: string): void {
     this.search.emit(value);
+  }
+
+  onKeyPress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
+  }
+
+  ngOnInit(): void {
+    this.debounceSubscription = this.debouncer
+      .pipe(debounceTime(500))
+      .subscribe((value => this.onDebounce.emit(value)));
+  }
+
+  ngOnDestroy(): void {
+    this.debounceSubscription?.unsubscribe();
   }
 }
