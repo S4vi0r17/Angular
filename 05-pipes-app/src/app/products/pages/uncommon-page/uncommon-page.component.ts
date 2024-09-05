5cffd8ba18a18d2064c3471c3ee34fa84af5de06
S4v!0r <gbp17@outlook.com>
2024-09-04 21:27:13 -0500

    Refactor uncommon-page component to add PrimeNG panel and cards for displaying transformed data

--- 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  // i18nSelect
+  public name: string = 'Gustavo';
+  public gender: 'male' | 'female' = 'male';
+  public genderMap = {
+    male: 'invite him',
+    female: 'invite her',
+  };
 
+  changeName(): void {
+    this.name = 'Viviana';
+    this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Gustavo', 'Viviana', 'Luis', 'Marta'];
+  public clientsMap = {
+    '=0': 'There are no clients waiting.',
+    '=1': 'There is one client waiting.',
+    other: 'There are # clients waiting.',
+  };
+
+  removeClient(): void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Gustavo',
+    age: 23,
+    address: '123 Street',
+  };
+
+  // Async Pipe
+  public myObservableTimer = interval(1000);
+  public promiseValue: Promise<string> = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 3000);
+  });
 }
